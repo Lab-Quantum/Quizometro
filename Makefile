51916f5039c8f1fc8ef3b01473ec1062866c9b0c
cristian <contato@cristiancunha.me>
2020-08-08 20:07:26 -0300

    Initial config

---
+++ Makefile
@@ -0,0 +1,11 @@
+lint:  ;@echo "Linting ${PROJECT}....."; \
+	./node_modules/.bin/eslint . --fix
+
+run: ;@echo "Starting ${PROJECT}....."; \
+	npm start
+
+install: ;@echo "Installing ${PROJECT}....."; \
+	npm install
+
+clean : ;
+	rm -rf node_modules
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "./assets/QuantumLogo.png";
+
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} alt="Quantum Logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
