e0e9e2b07a55cd1710d1e894127df6bd70e87961
lpeil <luan@peil.dev>
2020-08-09 18:26:59 -0300

    Create server structure

---
+++ server/headers.php
@@ -0,0 +1,7 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
