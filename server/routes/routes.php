9f9357d5fc667d92ff6fb66bc6db629fd8b27117
lpeil <luan@peil.dev>
2020-08-10 18:00:36 -0300

    Started server routes

---
+++ server/routes/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+use \System\Router;
+
+$routes = new Router();
---
+++ server/src/System/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace System;
+
+class Router {
+    public $routes = array();
+    public $params = array();
+
+    public function __construct() {
+        return true;
+    }
+}
