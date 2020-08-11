83b717b859dc0110f5ddacc201dc995d249cac80
Carrilhos <gcarrilhos@gmail.com>
2020-08-11 17:51:45 -0300

    Revert "create private route"
    This reverts commit 559f54381fe0215f56e202e6a1b97ecd84b5017a.

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,39 +1,23 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import CreateQuiz from "./pages/CreateQuiz";
-import ViewQuiz from "./pages/ViewQuiz";
-import EditQuiz from "./pages/EditQuiz";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CreateQuiz from './pages/CreateQuiz';
+import ViewQuiz from './pages/ViewQuiz';
+import EditQuiz from './pages/EditQuiz';
 
-import { isAuthenticated } from "./services/auth";
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      isAuthenticated() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: "/login", state: { from: props.location } }}
-        />
-      )
-    }
-  />
-);
 
 const Routes = () => {
-  return (
-    <Switch>
-      <Route component={Home} path="/" exact />
-      <Route component={Login} path="/login" />
-      <Route component={CreateQuiz} path="/createQuiz" />
-      <Route component={ViewQuiz} path="/viewQuiz" />
-      <Route component={EditQuiz} path="/editQuiz" />
-    </Switch>
-  );
-};
+    return(
+            <Switch>
+                <Route component={Home} path="/" exact />
+                <Route component={Login} path="/login" />
+                <Route component={CreateQuiz} path="/createQuiz" />
+                <Route component={ViewQuiz} path="/viewQuiz" />
+                <Route component={EditQuiz} path="/editQuiz" />
+            </Switch>
+    );
+}
 
 export default Routes;
