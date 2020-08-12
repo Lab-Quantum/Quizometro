f7b535354544023a557694bf9e58d0a0a59f2954
cristian <contato@cristiancunha.me>
2020-08-12 19:36:04 -0300

    Add dependences and apply 1º test

---
+++ web/cypress/integration/test/rotas.js
@@ -0,0 +1,22 @@
+describe('Teste de Rotas', () => {
+    it('Home', () => {
+      cy.visit('/');
+    });
+
+    it('Login', () => {
+      cy.visit('/login');
+    })
+
+    it('Create Quiz', () => {
+      cy.visit('/createQuiz');
+    })
+
+    it('View Quiz', () => {
+      cy.visit('/viewQuiz');
+    })
+
+    it('Edit Quiz', () => {
+      cy.visit('/editQuiz');
+    })
+  });
+  
