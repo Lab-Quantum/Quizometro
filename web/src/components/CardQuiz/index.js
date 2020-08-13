d5fdcf2a2a95b3b206552bda9e01f7923624acf6
cristian <contato@cristiancunha.me>
2020-08-13 20:35:57 -0300

    Some fixes

--- web/src/components/CardQuiz/index.js
+++ web/src/components/CardQuiz/index.js
@@ -6,25 +6,25 @@ import Button from '@material-ui/core/Button';
 const CardQuiz = () => {
     return(
         <div className="cardQuiz">
+            <div className="identifier">1</div>
             <div className="pictureContainer">
                 <img className="QuizImage" src="https://i1.wp.com/bastidoresdatv.com.br/wp-content/uploads/2014/05/gilberto-barros.jpg" alt="Quiz Picture" />
             </div>
             <div className="questionText">Quem é este deus?</div>
             <div className="alternatives">
                 <div className="alternative">
-                    <Button fullWidth variant="contained" color="primary" disableElevation>Preta Gil</Button>
+                    <Button fullWidth variant="contained" disableElevation>Preta Gil</Button>
                 </div>
                 <div className="alternative">
-                    <Button fullWidth variant="contained" color="primary" disableElevation>Niki Lauda</Button>
+                    <Button fullWidth variant="contained" disableElevation>Niki Lauda</Button>
                 </div>
                 <div className="alternative">
-                    <Button fullWidth variant="contained" color="primary" disableElevation>Gilberto Barros</Button>
+                    <Button fullWidth variant="contained" disableElevation>Gilberto Barros</Button>
                 </div>
                 <div className="alternative">
-                    <Button fullWidth variant="contained" color="primary" disableElevation>Cabeção da Malhação</Button>
+                    <Button fullWidth variant="contained" disableElevation>Cabeção da Malhação</Button>
                 </div>
             </div>
-            <div className="identifier">1</div>
         </div>
     )
 }
